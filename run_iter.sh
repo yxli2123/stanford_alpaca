609e6ac8e00b5e1cb07f070a5bc686edfd3f63cf
Yixiao Li, Castle <yxli2123@gmail.com>
2023-08-07 20:45:23 -0400

    fe

--- run_iter.sh
+++ run_iter.sh
@@ -1,5 +1,5 @@
-python iter_model.py --num_bits 2 --reduced_rank 8 --num_iter 1
 python iter_model.py --num_bits 2 --reduced_rank 8 --num_iter 0
+python iter_model.py --num_bits 2 --reduced_rank 8 --num_iter 1
 
 python iter_model.py --num_bits 2 --reduced_rank 16 --num_iter 0
 python iter_model.py --num_bits 2 --reduced_rank 16 --num_iter 1
@@ -7,14 +7,32 @@ python iter_model.py --num_bits 2 --reduced_rank 16 --num_iter 1
 python iter_model.py --num_bits 2 --reduced_rank 32 --num_iter 0
 python iter_model.py --num_bits 2 --reduced_rank 32 --num_iter 1
 
-python iter_model.py --num_bits 4 --reduced_rank 8 --num_iter 1
 python iter_model.py --num_bits 4 --reduced_rank 8 --num_iter 0
+python iter_model.py --num_bits 4 --reduced_rank 8 --num_iter 1
 
 python iter_model.py --num_bits 4 --reduced_rank 16 --num_iter 0
 python iter_model.py --num_bits 4 --reduced_rank 16 --num_iter 1
 
 python iter_model.py --num_bits 4 --reduced_rank 32 --num_iter 0
 python iter_model.py --num_bits 4 --reduced_rank 32 --num_iter 1
+
+python iter_model.py --num_bits 2 --reduced_rank 8 --num_iter 5
+python iter_model.py --num_bits 2 --reduced_rank 8 --num_iter 10
+
+python iter_model.py --num_bits 2 --reduced_rank 16 --num_iter 5
+python iter_model.py --num_bits 2 --reduced_rank 16 --num_iter 10
+
+python iter_model.py --num_bits 2 --reduced_rank 32 --num_iter 5
+python iter_model.py --num_bits 2 --reduced_rank 32 --num_iter 10
+
+python iter_model.py --num_bits 4 --reduced_rank 8 --num_iter 5
+python iter_model.py --num_bits 4 --reduced_rank 8 --num_iter 10
+
+python iter_model.py --num_bits 4 --reduced_rank 16 --num_iter 5
+python iter_model.py --num_bits 4 --reduced_rank 16 --num_iter 10
+
+python iter_model.py --num_bits 4 --reduced_rank 32 --num_iter 5
+python iter_model.py --num_bits 4 --reduced_rank 32 --num_iter 10
 
 #python iter_model.py --num_bits 2 --reduced_rank 8 --num_iter 10
 #python iter_model.py --num_bits 2 --reduced_rank 16 --num_iter 10
